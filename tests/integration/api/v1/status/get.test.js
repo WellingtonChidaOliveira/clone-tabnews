9e8f36bc66a95b21c113ae8107765c6c72dbc992
WellingtonChidaOliveira <97414459+WellingtonChidaOliveira@users.noreply.github.com>
2023-12-29 11:34:17 +0000

    'finish `/api/v1/status` endpoint'

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to /api/v1/status should returns 200", async () => {
   const resp = await fetch("http://localhost:3000/api/v1/status");
-  expect(resp.status).toBe(200);
+  const responseBody = await resp.json();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
